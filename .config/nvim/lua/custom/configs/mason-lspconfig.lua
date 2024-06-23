bd7ccd0e2aafc7c6facc21bed866f7f0f4a766f6
tkuramot <mabdman512@gmail.com>
2024-06-23 18:03:54 +0900

    update nvim lsp configs

--- .config/nvim/lua/custom/configs/mason-lspconfig.lua
+++ .config/nvim/lua/custom/configs/mason-lspconfig.lua
@@ -1,5 +1,6 @@
 local mason = require "mason"
 local mason_lspconfig = require "mason-lspconfig"
+local utils = require "core.utils"
 
 local lspconfig = require "lspconfig"
 local on_attach = require("plugins.configs.lspconfig").on_attach
@@ -12,17 +13,43 @@ mason_lspconfig.setup_handlers {
     lspconfig[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
-      root_dir = function()
-        return vim.loop.cwd()
+    }
+
+    lspconfig.gopls.setup {
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = true
+        client.server_capabilities.documentRangeFormattingProvider = true
       end,
+      settings = {
+        gopls = {
+          analyses = {
+            nilness = true,
+            unusedparams = true,
+            unusedwrite = true,
+            useany = true,
+          },
+          experimentalPostfixCompletions = true,
+          gofumpt = true,
+          staticcheck = true,
+          usePlaceholders = true,
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+        },
+      },
     }
 
     lspconfig.lua_ls.setup {
       capabilities = capabilities,
       on_attach = on_attach,
-      root_dir = function()
-        return vim.loop.cwd()
-      end,
       settings = {
         Lua = {
           diagnostics = {
@@ -35,14 +62,29 @@ mason_lspconfig.setup_handlers {
     lspconfig.tsserver.setup {
       capabilities = capabilities,
       on_attach = on_attach,
-      root_dir = function()
-        return vim.loop.cwd()
-      end,
+      root_dir = lspconfig.util.root_pattern "package.json",
       settings = {
         javascript = {
           format = {
             baseIndentSize = 2,
             convertTabsToSpaces = true,
+          },
+        },
+      },
+    }
+
+    lspconfig.denols.setup {
+      root_dir = lspconfig.util.root_pattern "deno.json",
+      init_options = {
+        lint = true,
+        unstable = true,
+        suggest = {
+          imports = {
+            hosts = {
+              ["https://deno.land"] = true,
+              ["https://cdn.nest.land"] = true,
+              ["https://crux.land"] = true,
+            },
           },
         },
       },
