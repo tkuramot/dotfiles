b0c92024d9e54a754388a973a7174a172aa9dcf9
tkuramot <taisei-kuramoto@freee.co.jp>
2026-07-27 11:10:29 +0900

    update aliases

--- .config/zsh/conf.d/03_aliases.zsh
+++ .config/zsh/conf.d/03_aliases.zsh
@@ -4,4 +4,5 @@ alias mkdir='mkdir -p'
 alias ..2='cd ../..'
 alias ..3='cd ../../..'
 
-alias cdr='cd $(ghq root)/$(ghq list | fzf)'
+alias cdg='cd $(ghq root)/$(ghq list | fzf)'
+alias cls='clear && printf "\033[3J"'
